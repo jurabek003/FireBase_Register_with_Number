4c4b5d0254ca572f31869f20513b2b6a47201d0a
Asus <turgunboyevjurabek777@gmail.com>
2023-07-12 05:33:59 +0500

    get sms finish!

--- app/src/main/java/uz/turgunboyevjurabek/firebase_register/MainActivity2.kt
+++ app/src/main/java/uz/turgunboyevjurabek/firebase_register/MainActivity2.kt
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
 import uz.turgunboyevjurabek.firebase_register.databinding.ActivityMain2Binding
+import java.util.concurrent.TimeUnit
 
+private const val TAG="MainActivity2"
 class MainActivity2 : AppCompatActivity() {
     private val binding by lazy { ActivityMain2Binding.inflate(layoutInflater) }
     private lateinit var auth: FirebaseAuth
     lateinit var storedVerificationId:String
     lateinit var handler: Handler
+    var count:Int=0
     lateinit var resentToken: PhoneAuthProvider.ForceResendingToken
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,13 +41,18 @@ class MainActivity2 : AppCompatActivity() {
                 "raqamiga yuborildi"
 
         handler= Handler(Looper.getMainLooper())
-        handler.postDelayed(runneble,3000)
-        if (count==60){
-            stopLockTask()
+        handler.postDelayed(runneble,1000)
+
+        sendVerificationCode(number)
+
+        binding.edtPossword.addTextChangedListener {
+            verifyCode()
         }
 
+        binding.refreshing.setOnClickListener {
+            sendVerificationCode(number)
+        }
     }
-    var count=0
     val runneble=object :Runnable{
         override fun run() {
             if (count<10){
@@ -46,8 +62,61 @@ class MainActivity2 : AppCompatActivity() {
             }
             count++
             handler.postDelayed(this,1000)
+            if (count>=60){
+                binding.thtTime.visibility=View.GONE
+                binding.refreshing.visibility=View.VISIBLE
+            }
         }
 
+    }
+    fun sendVerificationCode(phoneNumber:String){
+        val options=PhoneAuthOptions.newBuilder()
+            .setActivity(this)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L,TimeUnit.SECONDS)
+            .setCallbacks(callback)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private val callback=object :PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+        override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+            Log.d(TAG, "onVerificationCompleted: Uraaa")
+            Toast.makeText(this@MainActivity2, "callback", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onVerificationFailed(p0: FirebaseException) {
+            Toast.makeText(this@MainActivity2, "No callback ${p0.message}", Toast.LENGTH_LONG).show()
+            Log.d(TAG, "onVerificationCompleted:Failed",p0)
+        }
+
+        override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
+            Log.d(TAG, "onCodeSent: Kod jo'natilidi")
+            storedVerificationId=p0
+            resentToken=p1
+        }
+    }
+    private fun verifyCode(){
+        val code=binding.edtPossword.text.toString()
+        if (code.length==6){
+            val credential=PhoneAuthProvider.getCredential(storedVerificationId,code)
+            signInWithPhoneAuthCredential(credential)
+        }
+    }
+    fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential){
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Mufaqqiyatli", Toast.LENGTH_SHORT).show()
+
+                }else{
+                    Toast.makeText(this, "Mufaqiyatsiz", Toast.LENGTH_SHORT).show()
+                    if (it.exception is FirebaseAuthInvalidCredentialsException){
+                        Toast.makeText(this, "Kod hato kiritildi tekshirib qayta kiriting", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            }
     }
 
 }
